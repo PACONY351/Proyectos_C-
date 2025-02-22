90f6d8c73e6fc1b2ae040fff133403a3bd793afe
PACONY351 <robokop737@gmail.com>
2025-02-21 21:49:37 -0800

    [UPDATE] Pr3 Ej1

---
+++ Fundamentos_Basicos/Funciones/Pr3/Ej1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+
+void menu();
+void calcular(int);
+
+int main(){
+
+
+
+
+	return 0;
+}
+
+
+void menu(){
+	int segundos;
+	std::cout<<"Ingresa los segundos"<<std::endl;
+	std::cin>>segundos;
+	
+}
+
+void calcular(int segundos){
+	int dias, horas, minutos;
+	
+	if(segundos 
+}
