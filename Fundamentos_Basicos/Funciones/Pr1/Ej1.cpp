399d1d8c848d3c8679e251a0a598990001c760ea
PACONY351 <robokop737@gmail.com>
2025-02-21 21:11:58 -0800

    [UPDATE] Ej1.cpp

--- Fundamentos_Basicos/Funciones/Pr1/Ej1.cpp
+++ Fundamentos_Basicos/Funciones/Pr1/Ej1.cpp
@@ -4,6 +4,7 @@
 template <class TIPOD>
 void despliegue(TIPOD dato);
 
+int maximo(int n1, int n2, int n3);
 
 int main(){
 
@@ -16,11 +17,25 @@ int main(){
 	despliegue(dato2);
 	despliegue(dato3);
 	despliegue(dato4);
-
+	
+	std::cout<<"El numero maximo de: 20, 50, 44 "<<std::endl;
+	std::cout<<"es: "<<maximo(20, 50, 44);
+	std::cout<<std::endl;
+	
 	return 0;
 }
 
 template <class TIPOD>
 void despliegue(TIPOD dato){
 	std::cout<<"El dato es: "<<dato<<std::endl;
+}
+
+int maximo(int n1, int n2, int n3){
+	if(n1 > n2 && n1 > n3){
+		return n1;
+	}else if(n2 > n1 && n1 > n3){
+		return n2;
+	}else{
+		return n3;
+	}
 }
