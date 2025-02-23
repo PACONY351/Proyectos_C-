981626c91776a692e37a6d34077e75ae1ce55a07
PACONY351 <robokop737@gmail.com>
2025-02-23 09:15:15 -0800

    [UPDATE] Pr3 Ej1

--- Fundamentos_Basicos/Funciones/Pr3/Ej1.cpp
+++ Fundamentos_Basicos/Funciones/Pr3/Ej1.cpp
@@ -1,27 +1,34 @@
 #include<iostream>
 
-
 void menu();
-void calcular(int);
+void calcular(int, int&, int&, int&);
 
 int main(){
 
-
+	menu();
 
 
 	return 0;
 }
 
-
 void menu(){
-	int segundos;
+	int seg = 0, totalSeg = 0, horas = 0, min = 0;
 	std::cout<<"Ingresa los segundos"<<std::endl;
-	std::cin>>segundos;
+	std::cin>>totalSeg;
 	
+	calcular(totalSeg, horas, min, seg);
+
+	std::cout<<"Horas: "<<horas<<std::endl;
+	std::cout<<"Minutos: "<<min<<std::endl;
+	std::cout<<"Segundos: "<<seg<<std::endl;
 }
 
-void calcular(int segundos){
-	int dias, horas, minutos;
+void calcular(int totalSeg, int& horas, int& min, int& seg){
+	horas = totalSeg/3600;
+	totalSeg %= 3600;
 	
-	if(segundos 
+	min = totalSeg/60;
+
+	seg = totalSeg%60;
 }
+
