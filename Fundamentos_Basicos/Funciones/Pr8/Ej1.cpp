dd4408ddd54823bb7d4188003a50d253a925bfc3
PACONY351 <robokop737@gmail.com>
2025-02-25 10:03:00 -0800

    [ADD] Pr8 Ej1

---
+++ Fundamentos_Basicos/Funciones/Pr8/Ej1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+//PROTOTIPO DE FUNCIÓN
+void mostrarMatriz(int m[][3],int,int);
+//DATOS GLOBALES
+//MAIN
+int main(){
+	const int NFILAS = 2;
+	const int NCOL = 3;
+	int m[NFILAS][NCOL] = {{1,2,3},{4,5,6}};
+
+	return 0;
+}
+//FUNCIÓNES
