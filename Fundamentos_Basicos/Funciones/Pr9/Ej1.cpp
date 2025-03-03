703f77ade74fd425c6c15da0ea3c4f43eac36cdc
PACONY351 <robokop737@gmail.com>
2025-03-03 01:20:09 -0800

    [ADD] Pr9 Ej1

---
+++ Fundamentos_Basicos/Funciones/Pr9/Ej1.cpp
@@ -0,0 +1,23 @@
+#include<iostrem>
+
+
+
+struct Complejo{
+	float real,imaginaria;
+}z1,z2;
+
+
+void pedirDatos();
+Complejo suma(Complejo, Complejo);
+
+
+int main(){
+	
+	pedirDatos();
+	
+	
+	return 0;
+}
+void pedirDatos(){
+	std::cout<<""
+}
