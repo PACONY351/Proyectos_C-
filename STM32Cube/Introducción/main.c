416f949a8ce058a09f83fbb1270666c2fe25cd21
PACONY351 <robokop737@gmail.com>
2025-03-23 22:03:18 -0700

    [ADD] STM32Cube/Introducción/Introduccion*

---
+++ STM32Cube/Introducción/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 23, 2025
+ *      Author: franciso
+ */
+#include<stdio.h>
+	void menuPrincipal();
+	void opcionesPrincipales();
+int main(){
+	menuPrincipal();
+
+	return 0;
+}
+void menuPrincipal(){
+	printf("+-----------Menu------------+\n");
+
+}
