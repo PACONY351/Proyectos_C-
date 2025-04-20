e9668f17514cd5ab8fceb03980e486cd847e1c13
PACONY351 <robokop737@gmail.com>
2025-04-20 13:36:01 -0600

    [ADD] Arduino/Pr4 Control_datos_por_Wifi *

--- Arduino ESP32/Pr4 Datos_por_Wifi/sketch_apr20a/sketch_apr20a.ino
+++ Arduino ESP32/Pr4 Datos_por_Wifi/sketch_apr20a/sketch_apr20a.ino
@@ -1,28 +1,41 @@
 
+// Librerias- Ubidots
 #include<Ubidots.h>
 
-const char* UBIDOTS_TOKEN = "";
-const char* WIFI_SSID = "";
-const char* WIFI_PASS = "";
+// Declaracion de constantes char tipo puntero con token y red Wifi 
+const char* UBIDOTS_TOKEN = "BBUS-NtBXDgeRZAniKmWWYyy508FERbab5i";
+const char* WIFI_SSID = "IZZI-C84D";
+const char* WIFI_PASS = "50955127C84D";
 
+// Creacion del objeto Ubidots con parametros 
 Ubidots ubidots(UBIDOTS_TOKEN, UBI_HTTP);
 
-
+// Variables
 int var_poten;
 
 
 
 void setup() {
 
+  // Velocidad del Serial
   Serial.begin(115200);
+  // Iniciar la conexion a la red Wifi
   ubidots.wifiConnect(WIFI_SSID, WIFI_PASS);
-  var_poter = 0;
+  var_poten = 0;
 }
 
 void loop() {
   
-  var_poter = analogRead(A0);
+  var_poten = analogRead(A0);
   Serial.println(var_poten);
 
   ubidots.add("var_poten", var_poten);
+
+  bool bufferSet = false;
+  bufferSet = ubidots.send();
+
+  if(bufferSet){
+    Serial.println("Values sent by the divice");
+  }
+  delay(1000);
 }
